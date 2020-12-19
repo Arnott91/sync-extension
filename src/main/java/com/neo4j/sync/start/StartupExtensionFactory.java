98f8babdfe5026d36c11523900074b6341e11755
Jim Webber <jim@jimwebber.org>
2020-12-19 15:35:12 +0000

    Correct class name in strings

--- src/main/java/com/neo4j/sync/start/StartupExtensionFactory.java
+++ src/main/java/com/neo4j/sync/start/StartupExtensionFactory.java
@@ -13,12 +13,12 @@ import org.neo4j.logging.internal.LogService;
 public class StartupExtensionFactory extends ExtensionFactory<StartupExtensionFactory.Dependencies> {
 
     public StartupExtensionFactory() {
-        super(ExtensionType.DATABASE, "AutoStartExtensionFactory");
+        super(ExtensionType.DATABASE, "StartupExtensionFactory");
     }
 
     @Override
     public Lifecycle newInstance(ExtensionContext extensionContext, Dependencies dependencies) {
-        dependencies.log().getUserLog(this.getClass()).info("AutoStartExtensionFactory#newInstance");
+        dependencies.log().getUserLog(this.getClass()).info("StartupExtensionFactory#newInstance");
         return new Startup(dependencies);
     }
 
