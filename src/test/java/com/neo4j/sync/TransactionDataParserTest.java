a649a59ebde135de1862ac77a5162e43cbead7eb
cupkes <chris.upkes@neo4j.com>
2021-01-05 11:40:29 -0500

    fixed the data parser tests

--- src/test/java/com/neo4j/sync/TransactionDataParserTest.java
+++ src/test/java/com/neo4j/sync/TransactionDataParserTest.java
@@ -1,17 +1,22 @@
 package com.neo4j.sync;
 
+import com.neo4j.sync.engine.NodeDirection;
+import com.neo4j.sync.engine.NodeFinder;
 import com.neo4j.sync.engine.TransactionDataParser;
 import org.codehaus.jettison.json.JSONArray;
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 import org.junit.jupiter.api.Test;
+import org.neo4j.graphdb.Label;
+import org.neo4j.graphdb.Transaction;
+import scala.concurrent.java8.FuturesConvertersImpl;
 
+import javax.inject.Inject;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TransactionDataParserTest {
 
@@ -19,6 +24,8 @@ public class TransactionDataParserTest {
     private final String ADD_NODES_AND_PROPERTIES = "{\"transactionEvents\":[{\"changeType\":\"AddNode\",\"nodeLabels\":[\"Test\"],\"primaryKey\":{\"uuid\":\"123XYZ\"},\"nodeKey\":null,\"relationshipLabel\":null,\"targetNodeLabels\":null,\"targetPrimaryKey\":null,\"properties\":null,\"allProperties\":{\"testProperty\":\"foo\",\"uuid\":\"123XYZ\"},\"uuid\":null,\"timestamp\":null,\"transactionId\":null,\"targetNodeKey\":null},{\"changeType\":\"AddNode\",\"nodeLabels\":[\"Test\"],\"primaryKey\":{\"uuid\":\"XZY123\"},\"nodeKey\":null,\"relationshipLabel\":null,\"targetNodeLabels\":null,\"targetPrimaryKey\":null,\"properties\":null,\"allProperties\":{\"testProperty\":\"bar\",\"uuid\":\"XZY123\"},\"uuid\":null,\"timestamp\":null,\"transactionId\":null,\"targetNodeKey\":null}]}";
     private final String ADD_MULTIPLE_RELATIONSHIPS = "{\"transactionEvents\":[{\"changeType\":\"AddRelation\",\"nodeLabels\":[\"Test\"],\"primaryKey\":{\"uuid\":\"123XYZ\"},\"nodeKey\":null,\"relationshipLabel\":\"CONNECTED_TO\",\"targetNodeLabels\":[\"Test\"],\"targetPrimaryKey\":{\"uuid\":\"XYZ123\"},\"properties\":null,\"allProperties\":{},\"uuid\":null,\"timestamp\":null,\"transactionId\":null,\"targetNodeKey\":null},{\"changeType\":\"AddRelation\",\"nodeLabels\":[\"Test\"],\"primaryKey\":{\"uuid\":\"123XYZ\"},\"nodeKey\":null,\"relationshipLabel\":\"LIKES\",\"targetNodeLabels\":[\"Movie\"],\"targetPrimaryKey\":{\"uuid\":\"ABC\"},\"properties\":null,\"allProperties\":{},\"uuid\":null,\"timestamp\":null,\"transactionId\":null,\"targetNodeKey\":null},{\"changeType\":\"AddNode\",\"nodeLabels\":[\"Test\"],\"primaryKey\":{\"uuid\":\"123XYZ\"},\"nodeKey\":null,\"relationshipLabel\":null,\"targetNodeLabels\":null,\"targetPrimaryKey\":null,\"properties\":null,\"allProperties\":{\"uuid\":\"123XYZ\"},\"uuid\":null,\"timestamp\":null,\"transactionId\":null,\"targetNodeKey\":null},{\"changeType\":\"AddNode\",\"nodeLabels\":[\"Test\"],\"primaryKey\":{\"uuid\":\"XYZ123\"},\"nodeKey\":null,\"relationshipLabel\":null,\"targetNodeLabels\":null,\"targetPrimaryKey\":null,\"properties\":null,\"allProperties\":{\"uuid\":\"XYZ123\"},\"uuid\":null,\"timestamp\":null,\"transactionId\":null,\"targetNodeKey\":null},{\"changeType\":\"AddNode\",\"nodeLabels\":[\"Movie\"],\"primaryKey\":{\"uuid\":\"ABC\"},\"nodeKey\":null,\"relationshipLabel\":null,\"targetNodeLabels\":null,\"targetPrimaryKey\":null,\"properties\":null,\"allProperties\":{\"uuid\":\"ABC\"},\"uuid\":null,\"timestamp\":null,\"transactionId\":null,\"targetNodeKey\":null}]}";
     private final String NODE_PROPERTY_CHANGE = "{\"transactionEvents\":[{\"changeType\":\"NodePropertyChange\",\"nodeLabels\":[\"Test\"],\"primaryKey\":{\"uuid\":\"123XYZ\"},\"nodeKey\":null,\"relationshipLabel\":null,\"targetNodeLabels\":null,\"targetPrimaryKey\":null,\"properties\":[{\"propertyName\":\"test\",\"oldValue\":\"foo\",\"newValue\":null}],\"allProperties\":{\"uuid\":\"123XYZ\"},\"uuid\":null,\"timestamp\":null,\"transactionId\":null,\"targetNodeKey\":null}]}";
+    @Inject
+    private List<Map<String, JSONObject>> transactionEvents;
 
     @Test
     void translateJSONTest() throws JSONException {
@@ -29,61 +36,89 @@ public class TransactionDataParserTest {
     @Test
     void addNodeTest() throws JSONException {
 
-        JSONObject graphTxTranslation = TransactionDataParser.TranslateTransactionData(ADD_NODE);
-        String changeType = graphTxTranslation.getString("changeType");
-        assertEquals("AddNode", changeType);
+        this.transactionEvents = TransactionDataParser.getTransactionEvents(new JSONObject(ADD_NODE));
+        Map<String,JSONObject> event = transactionEvents.get(0);
+        for (Map.Entry<String, JSONObject> e : event.entrySet()) {
+            String k = e.getKey();
+            assertEquals("AddNode", k);
+            JSONObject v = e.getValue();
+            String[] labels = (String[]) TransactionDataParser.getNodeLabels(v);
+            assertEquals("Test", labels[0]);
+            // get the collection of properties
+            Map<String, Object> properties = TransactionDataParser.getNodeProperties(v);
+            assertEquals("123XYZ",properties.get("uuid"));
 
-        JSONObject allProperties = new JSONObject(graphTxTranslation.get("allProperties").toString());
+            }
 
-        assertEquals(1, allProperties.length());
-        assertEquals("123XYZ", allProperties.get("uuid"));
     }
 
     @Test
     void addNodeAndPropertyTest() throws JSONException {
 
-        JSONObject graphTxTranslation = TransactionDataParser.TranslateTransactionData(ADD_NODES_AND_PROPERTIES);
-        String changeType = graphTxTranslation.getString("changeType");
-        assertEquals("AddNode", changeType);
+        this.transactionEvents = TransactionDataParser.getTransactionEvents(new JSONObject(ADD_NODES_AND_PROPERTIES));
+        Map<String,JSONObject> event = transactionEvents.get(0);
+        for (Map.Entry<String, JSONObject> e : event.entrySet()) {
+            String k = e.getKey();
+            assertEquals("AddNode", k);
+            JSONObject v = e.getValue();
+            String[] labels = (String[]) TransactionDataParser.getNodeLabels(v);
+            assertEquals("Test", labels[0]);
+            // get the collection of properties
+            Map<String, Object> properties = TransactionDataParser.getNodeProperties(v);
+            assertEquals("123XYZ",properties.get("uuid"));
+            assertEquals("foo",properties.get("testProperty"));
 
-        JSONObject allProperties = (JSONObject) graphTxTranslation.get("allProperties");
+        }
 
-        assertEquals(2, allProperties.length());
-        assertEquals("foo", allProperties.get("testProperty"));
     }
 
     @Test
     void addMultipleRelationshipsTest() throws JSONException {
 
-        JSONObject graphTxTranslation = TransactionDataParser.TranslateTransactionData(ADD_MULTIPLE_RELATIONSHIPS);
+        this.transactionEvents = TransactionDataParser.getTransactionEvents(new JSONObject(ADD_MULTIPLE_RELATIONSHIPS));
+        Map<String,JSONObject> event = transactionEvents.get(0);
+        for (Map.Entry<String, JSONObject> e : event.entrySet()) {
+            String k = e.getKey();
+            JSONObject v = e.getValue();
+            if (k.equals("AddRelation")) {
 
-        JSONObject allProperties = (JSONObject) graphTxTranslation.get("allProperties");
+                NodeFinder finder = new NodeFinder(v);
+                Label startSearchLabel = finder.getSearchLabel(NodeDirection.START);
+                Label targetSearchLabel = finder.getSearchLabel(NodeDirection.TARGET);
 
-        assertEquals(2, allProperties.length());
-        assertEquals("foo", allProperties.get("testProperty"));
+                String[] startPrimaryKey = finder.getPrimaryKey(NodeDirection.START);
+                String[] targetPrimaryKey = finder.getPrimaryKey(NodeDirection.TARGET);
+                assertEquals("uuid",startPrimaryKey[0]);
+                assertEquals("uuid",targetPrimaryKey[0]);
+                assertEquals("Test",startSearchLabel.toString());
+                // Map<String, Object> properties = TransactionDataParser.getRelationProperties(v);
+            } else {
+                String[] labels = (String[]) TransactionDataParser.getNodeLabels(v);
+                assertEquals("Test", labels[0]);
+                // get the collection of properties
+                Map<String, Object> properties = TransactionDataParser.getNodeProperties(v);
+                assertEquals("123XYZ",properties.get("uuid"));
+            }
+        }
 
     }
 
     @Test
-    void getRemovedNodesTest() throws JSONException {
+    void getRemovedNodePropTest() throws JSONException {
 
-        JSONObject graphTxTranslation = TransactionDataParser.TranslateTransactionData(NODE_PROPERTY_CHANGE);
+        this.transactionEvents = TransactionDataParser.getTransactionEvents(new JSONObject(NODE_PROPERTY_CHANGE));
+        Map<String,JSONObject> event = transactionEvents.get(0);
+        for (Map.Entry<String, JSONObject> e : event.entrySet()) {
+            String k = e.getKey();
+            assertEquals("NodePropertyChange", k);
+            JSONObject v = e.getValue();
+            String[] labels = (String[]) TransactionDataParser.getNodeLabels(v);
+            assertEquals("Test", labels[0]);
+            // get the collection of properties
+            String[] properties = TransactionDataParser.getRemovedProperties(v);
+            assertEquals("test", properties[0]);
 
-        List<Map<String, JSONObject>> txEvents = TransactionDataParser.getTransactionEvents(graphTxTranslation);
-
-        JSONObject propertyChangeEvent = txEvents.get(0).get("NodePropertyChange");
-
-        JSONArray beforeAndAfterArray = propertyChangeEvent.getJSONArray("properties");
-
-        List<String> removed = new ArrayList<>();
-
-        for (int i = 0; i < beforeAndAfterArray.length(); i++) {
-
-            JSONObject banda = (JSONObject) beforeAndAfterArray.get(i);
-            if (banda.get("newValue").equals(null)) {
-                removed.add(banda.get("propertyName").toString());
-
-            }
         }
+
     }
 }
